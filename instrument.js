b89b7555c75e97e3ea6c1ee6c68d917cfdec7371
Daniel Neville <danielneville@Daniels-Mini.lan>
2025-05-15 18:40:44 -0400

    🔐 Update .gitignore to ignore .env and 🔧 update Sentry DSN

--- instrument.js
+++ instrument.js
@@ -1,6 +1,10 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
 const Sentry = require("@sentry/node");
 
 Sentry.init({
-  dsn: "https://5d089fcb6d78dbb43b7027528bb267c0@o4590320267431936.ingest.us.sentry.io/4509320299872256",
-  sendDefaultPii: true, // Sends PII like IP address (optional)
+  dsn: "https://4af2b8f6f0e47217a6f0d36e5533346b@o4509320267431936.ingest.us.sentry.io/4509329549099008",
+
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
 });
